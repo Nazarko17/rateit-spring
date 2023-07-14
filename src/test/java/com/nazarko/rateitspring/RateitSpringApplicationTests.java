165ad3232e183b8e10d1a0cfd9db6a6d74ba9e19
Nazar <tjnazar1999@gmail.com>
2023-07-14 16:12:41 +0300

    Initial commit

---
+++ src/test/java/com/nazarko/rateitspring/RateitSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nazarko.rateitspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateitSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
